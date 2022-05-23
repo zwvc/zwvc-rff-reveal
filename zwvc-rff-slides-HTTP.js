9342bbc0bbb24b1846570b34e4012482d83f6b80
zwvc <zwvc@163.com>
2022-05-23 20:59:05 +0800

    Add HTTP File Server Page

---
+++ zwvc-rff-slides-HTTP.js
@@ -0,0 +1,362 @@
+zwvc_base_dir = "http://omv-buster.local:8888/";
+document.write(`
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/封面_921x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/01_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/02_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/03_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/04_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/05_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="SeaShell" data-transition="convex">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/06_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="SeaShell" data-transition="convex">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/07_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/08_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/09_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/10_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="white" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/11_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/12_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/13_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/14_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/15_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/16_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/17_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/18_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/19_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="LightGray" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/20_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="white" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/21_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/22_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/23_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="white" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/24_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="white" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/25_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="zoom">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/26_1600x1023.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/27_1600x1023.jpg">
+				</section>
+				<section data-background="black" data-transition="slide">
+					<div class="r-stack">
+						<div class="r-hstack" style="width:1024px; height:600px;">
+						  <img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0853﹣北京_800x1200.jpg">
+						  <img class="r-stretch fragment fade-left" data-src="${zwvc_base_dir}/諾﹣IMG_0902﹣北京_800x1200.jpg">
+						</div>
+					</div>
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0868﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0869﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0886﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0887﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0916﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0918﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0927﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0936﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0942﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0950﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0953﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_0960﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1015﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1020﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1023﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1024﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1028﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1032﹣北京A_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1032﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1034﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1073﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1077﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1082﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1089﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1097﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1101﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1103﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1106﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1121﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1127﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1128﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1130﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1138﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1146﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1149﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1150﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1151﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1155﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1163﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1173﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1182﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1183﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1185﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1186﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="concave">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1196﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="concave">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1201﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="concave">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1202﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="concave">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1206﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="DarkSlateGray" data-transition="concave">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1212﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1221﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1224﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1228﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1237﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1239﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1240﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1241﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1245﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1249﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1253﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1257﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1261﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1268﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1279﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1285﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1318﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1322﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1331﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1346﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1354﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1357﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1361﹣北京_1200x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1371﹣北京_1200x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1372﹣北京_1200x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1378﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1385﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1390﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1402﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1419﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1430﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1455﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1491﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1496﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1498﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1500﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1503﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1531﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="tan" data-transition="slide">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1532﹣北京_1600x1067.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1575﹣北京_800x1200.jpg">
+				</section>
+				<section data-fullscreen data-background="black">
+					<img class="r-stretch" data-src="${zwvc_base_dir}/諾﹣IMG_1576﹣北京_800x1200.jpg">
+				</section>
+`);
